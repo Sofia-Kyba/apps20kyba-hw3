bfc3c04f3bf74a2c0d07c9b72be37ff9abcecb06
Sofia Kyba <56972348+Sofia-Kyba@users.noreply.github.com>
2020-11-27 21:27:40 +0200

    Update SortDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/SortDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/SortDecorator.java
@@ -1,12 +1,10 @@
 package ua.edu.ucu.smartarr;
 
 import ua.edu.ucu.functions.MyComparator;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 // Sorts elements using MyComparator to compare them
-public class SortDecorator extends SmartArrayDecorator{
+public class SortDecorator extends SmartArrayDecorator {
 
     private MyComparator comparator;
     private int size;
